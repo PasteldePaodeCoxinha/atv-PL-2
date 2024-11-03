599e2b7e69053d36213d8299d1e1d9eb44b82e1e
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-02 22:21:44 -0300

    Mudei a pagina de alterar serviço

--- src/componentes/servico/alterar/alterarServico.tsx
+++ src/componentes/servico/alterar/alterarServico.tsx
@@ -16,16 +16,45 @@ export default class AlterarServico extends Component<props, state> {
         this.state = {
             servico: props.servico
         }
+        this.mudarValorNome = this.mudarValorNome.bind(this)
+        this.mudarValorPreco = this.mudarValorPreco.bind(this)
+    }
+
+    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
+        const servico = this.state.servico
+        servico.nome = e.target.value
+        this.setState({
+            servico: servico
+        })
+    }
+
+    mudarValorPreco(e: React.ChangeEvent<HTMLInputElement>) {
+        const servico = this.state.servico
+        servico.preco = Number(e.target.value).valueOf()
+        this.setState({
+            servico: servico
+        })
     }
 
     render() {
-        return (<div className="informarcoesDeUmServicoNaLista">
-            <ul className="listaDeInformacoesServico">
-                <li> <label htmlFor="">Nome:</label> <input type="text" value={this.state.servico.nome} /></li>
-                <li><label htmlFor="">Preço:</label> <input type="number" value={this.state.servico.preco} /> </li>
+        return (
+            <div className="containerInformacoesServico">
+                <div className="campoServicoEditavel">
+                    <label>Nome:</label>
+                    <input type="text" value={this.state.servico.nome} onChange={this.mudarValorNome} />
+                </div>
 
-                <li><label htmlFor="">Qtd vendidos:</label> <input type="text" value={this.state.servico.getCompraram} /> </li>
-            </ul>
-        </div>)
+                <div className="campoServicoEditavel">
+                    <label>Tamanho:</label>
+                    <input type="number" value={this.state.servico.preco} onChange={this.mudarValorPreco} />
+                </div>
+
+                <div className="campoServicoFixo">
+                    <label>Quantidade vendido:</label>
+                    <p>{this.state.servico.getCompraram}</p>
+                </div>
+
+            </div>
+        )
     }
 }
