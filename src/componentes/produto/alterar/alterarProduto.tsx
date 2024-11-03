1013b2ef943a0eda091fa3fe0ecbd9b14076eecd
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-02 22:18:21 -0300

    Mudei a pagina de alterar produto

--- src/componentes/produto/alterar/alterarProduto.tsx
+++ src/componentes/produto/alterar/alterarProduto.tsx
@@ -16,16 +16,46 @@ export default class AlterarProduto extends Component<props, state> {
         this.state = {
             produto: props.produto
         }
+
+        this.mudarValorNome = this.mudarValorNome.bind(this)
+        this.mudarValorPreco = this.mudarValorPreco.bind(this)
+    }
+
+    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
+        const produto = this.state.produto
+        produto.nome = e.target.value
+        this.setState({
+            produto: produto
+        })
+    }
+
+    mudarValorPreco(e: React.ChangeEvent<HTMLInputElement>) {
+        const produto = this.state.produto
+        produto.preco = Number(e.target.value).valueOf()
+        this.setState({
+            produto: produto
+        })
     }
 
     render() {
-        return (<div className="informarcoesDeUmProdutoNaLista">
-            <ul className="listaDeInformacoesProduto">
-                <li> <label htmlFor="">Nome:</label> <input type="text" value={this.state.produto.nome} /></li>
-                <li><label htmlFor="">Preço:</label> <input type="number" value={this.state.produto.preco} /> </li>
+        return (
+            <div className="containerInformacoesProduto">
+                <div className="campoProdutoEditavel">
+                    <label>Nome:</label>
+                    <input type="text" value={this.state.produto.nome} onChange={this.mudarValorNome} />
+                </div>
 
-                <li><label htmlFor="">Qtd vendidos:</label> <input type="text" value={this.state.produto.getCompraram} /> </li>
-            </ul>
-        </div>)
+                <div className="campoProdutoEditavel">
+                    <label>Tamanho:</label>
+                    <input type="number" value={this.state.produto.preco} onChange={this.mudarValorPreco} />
+                </div>
+
+                <div className="campoProdutoFixo">
+                    <label>Quantidade vendido:</label>
+                    <p>{this.state.produto.getCompraram}</p>
+                </div>
+
+            </div>
+        )
     }
 }
